fcd81c5113d8d253c665af2104ea3c1389c3a4dd
OptixWolf <62619140+OptixWolf@users.noreply.github.com>
2023-11-06 22:11:20 +0100

    Update README.md

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:projekt_archive/main.dart';
+import 'package:Archiv/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
